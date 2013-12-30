a8459a88d3148a544a56505c49c1bd2edf325b40
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2013-12-30 11:52:15 -0800

    Bug fixes in the parser

--- src/main/java/bayonet/coda/CodaParser.java
+++ src/main/java/bayonet/coda/CodaParser.java
@@ -2,6 +2,7 @@ package bayonet.coda;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,7 +13,6 @@ import briefj.BriefIO;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import com.google.common.io.Files;
 
 import static briefj.BriefIO.*;
 
@@ -20,40 +20,88 @@ import static briefj.BriefIO.*;
 
 public class CodaParser
 {
-  
-  
   public static void fromCoda(File codaIndex, File codaContents, File destinationDirectory)
   {
     BriefIO.createParentDirs(destinationDirectory);
     LinkedList<Block> blocks = readCodaIndex(codaIndex);
     
+    // state of the writing process:
     int nLinesLeftInBlock = 0;
     Block currentBlock = null;
     PrintWriter out = null;
+    
     for (String line : readLines(codaContents))
     {
       if (nLinesLeftInBlock == 0)
       {
-        if (out != null) out.close();
+        // a new block is starting
         Block newBlock = blocks.pollFirst();
-        nLinesLeftInBlock = currentBlock.nSamples;
+        nLinesLeftInBlock = newBlock.nSamples;
         
+        // only need to change the output file if starting (currentBlock == null)
+        // or if the variable name changes (vs sometimes only the indices change)
         if (currentBlock == null || !currentBlock.variableName.equals(newBlock.variableName))
-          out = output(new File(destinationDirectory, currentBlock.variableName));
+        {
+          if (out != null) out.close();
+          out = output(new File(destinationDirectory, newBlock.variableName + ".csv"));
+        }
         
         currentBlock = newBlock;
       }
-      Scanner lineScanner = new Scanner(line);
-      @SuppressWarnings("unused")
-      String iterationIndex = lineScanner.next(); // not used
-      double value = lineScanner.nextDouble(); // payload
+      
+      // parse one point
+//      Scanner lineScanner = new Scanner(line).useDelimiter("\\s+");
+//      @SuppressWarnings("unused")
+//      String iterationIndex = lineScanner.next(); // we do not need to use this field
+      String [] fields = line.split("\\s+");
+      double value = Double.parseDouble(fields[1]);//lineScanner.nextDouble(); // payload
       for (int index : currentBlock.indices)
         out.print(index + ",");
       out.println(value);
       nLinesLeftInBlock--;
     }
     if (out != null) out.close();
+  }
+  
+  public static void toCoda(File codaIndex, File codaContents, File variableFilesDirectory)
+  {
+    PrintWriter codaIndexOut =  output(codaIndex);
+    PrintWriter codaContentsOut=output(codaContents);
     
+    int currentLine = 0;
+    int startOfBlockLine = 0;
+    for (File variableFile : ls(variableFilesDirectory, "csv"))
+    {
+      String variableName = variableFile.getName().replace(".csv", "");
+      List<String> previousIndices = null;
+      int currentMCMCIter = 0;
+      for (List<String> fields : readLines(variableFile).splitCSV())
+      {
+        List<String> currentIndices = fields.subList(0, Math.max(0, fields.size() - 2));
+        if (previousIndices == null) previousIndices = currentIndices;
+        if (!previousIndices.equals(currentIndices))
+        {
+          codaIndexOut.println(codeIndexLine(variableName, previousIndices, startOfBlockLine, currentLine - 1));
+          previousIndices = currentIndices;
+          startOfBlockLine = currentLine;
+        }
+        codaContentsOut.println("" + (currentMCMCIter+1) + "  " + fields.get(fields.size() - 1));
+        currentLine++;
+        currentMCMCIter++;
+      }
+      codaIndexOut.println(codeIndexLine(variableName, previousIndices, startOfBlockLine, currentLine - 1));
+    }
+    
+    codaIndexOut.close();
+    codaContentsOut.close();
+  }
+  
+  private static String codeIndexLine(String variableName, List<String> previousIndices, int startOfBlockLine, int endOfBlockLine)
+  {
+    return "" + 
+      variableName + 
+      (previousIndices.isEmpty() ? "" : "[" + Joiner.on(",").join(previousIndices) + "]") +
+      " " + (startOfBlockLine + 1) + " " + (endOfBlockLine + 1);
   }
   
   private static LinkedList<Block> readCodaIndex(File codaIndex)
@@ -61,18 +109,20 @@ public class CodaParser
     LinkedList<Block> result = Lists.newLinkedList();
     for (String line : readLines(codaIndex))
     {
-      Scanner lineScanner = new Scanner(line);
-      List<String> variableAndIndex = on("\\]")
-        .splitToList(lineScanner.next().replace("]",""));
+//      Scanner lineScanner = new Scanner(line);
+      String[] fields = line.split("\\s+");
+      String[] variableAndIndex = fields[0].replace("]", "").split("\\["); //on("\\[")
+//        .splitToList(fields[0].replace("]",""));
       final int 
-        firstInclusive = lineScanner.nextInt(),
-        secondInclusive= lineScanner.nextInt();
+        firstInclusive = Integer.parseInt(fields[1]), //lineScanner.nextInt(),
+        secondInclusive= Integer.parseInt(fields[2]); //lineScanner.nextInt();
       final int nSamples = secondInclusive - firstInclusive + 1;
       
-      String variable = variableAndIndex.get(0);
-      int [] indices = new int[variableAndIndex.size() - 1];
+      String variable = variableAndIndex[0]; //variableAndIndex.get(0);
+      String [] indicesStr = variableAndIndex.length == 2 ? variableAndIndex[1].split(",") : new String[0];
+      int [] indices = new int[indicesStr.length];
       for (int i = 0; i < indices.length; i++)
-        indices[i] = Integer.parseInt(variableAndIndex.get(i+1));
+        indices[i] = Integer.parseInt(indicesStr[i]);
       
       result.add(new Block(variable, indices, nSamples));
     }
@@ -90,6 +140,26 @@ public class CodaParser
       this.indices = indices;
       this.nSamples = nSamples;
     }
+    @Override
+    public String toString()
+    {
+      return "Block [variableName=" + variableName + ", indices="
+          + Arrays.toString(indices) + ", nSamples=" + nSamples + "]";
+    }
+  }
+  
+  public static void main(String [] args)
+  {
+    File originalCodaIndex = new File("/Users/bouchard/temp/CODAindex.txt");
+    File originalCoda = new File("/Users/bouchard/temp/CODAchain1.txt");
+    
+    File dest = new File("/Users/bouchard/temp/created");
+    
+    fromCoda(originalCodaIndex, originalCoda, dest);
+    
+    File newCodaIndex = new File("/Users/bouchard/temp/created2/CODAindex.txt");
+    File newCoda = new File("/Users/bouchard/temp/created2/CODAchain1.txt");
+    toCoda(newCodaIndex, newCoda, dest);
   }
   
 }
