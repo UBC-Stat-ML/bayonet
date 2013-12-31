103a614dd5a01bf59704faeb74f9f557ec7769fc
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2013-12-31 05:44:59 -0800

    Added document skeleton

---
+++ src/test/java/bayonet/bugs/TestWrapper.java
@@ -0,0 +1,23 @@
+package bayonet.bugs;
+
+import org.junit.Test;
+
+import tutorialj.Tutorial;
+
+
+
+public class TestWrapper
+{
+  /**
+   * Bugs wrapper
+   * ------------
+   * 
+   * This module makes it easier to call JAGS.
+   */
+  @Test
+  @Tutorial
+  public void test()
+  {
+    
+  }
+}
