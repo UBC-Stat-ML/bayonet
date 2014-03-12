5072714586fc2af6590dcc24bddcbbdb93e1ef5d
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-03-12 14:56:58 -0700

    Adding simple eigen-decomposition access to EJML.

--- src/main/java/bayonet/math/EJMLUtils.java
+++ src/main/java/bayonet/math/EJMLUtils.java
@@ -1,36 +1,148 @@
 package bayonet.math;
 
 
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.DecompositionFactory;
+import org.ejml.factory.EigenDecomposition;
+import org.ejml.ops.EigenOps;
+import org.ejml.ops.EjmlUnitTests;
 import org.ejml.simple.SimpleMatrix;
 
 import briefj.BriefIO;
+import briefj.BriefStrings;
 
 
-
+/**
+ * 
+ * Utilities around functionalities in the Efficient Java Matrix Library.
+ * 
+ * 
+ * @author Alexandre Bouchard (alexandre.bouchard@gmail.com)
+ *
+ */
 public class EJMLUtils
 {
   /**
-   * TODO: rename to l2NormSquared
-   * TODO: check EJML does not already include that
+   * A convenient access to EJML's eigen decomposition functionality.
+   * Note that using the matrix directly is more efficient if many eigendecompositions
+   * need to be performed.
+   * 
+   * Also checks that V D V^{-1} is indeed equal to the original matrix up
+   * to numerical error (1e-8).
+   * 
+   * @param matrix
+   * @return
+   */
+  public static SimpleEigenDecomposition simpleEigenDecomposition(SimpleMatrix matrix)
+  {
+    EigenDecomposition<DenseMatrix64F> eigenDecomp = DecompositionFactory.eig(matrix.numCols(), true);
+    if (!eigenDecomp.decompose(matrix.getMatrix()))
+      throw new RuntimeException();
+    SimpleMatrix D = new SimpleMatrix(EigenOps.createMatrixD(eigenDecomp));
+    SimpleMatrix V = new SimpleMatrix(EigenOps.createMatrixV(eigenDecomp));
+    SimpleMatrix Vinverse = V.invert();
+    test(matrix,V,D,Vinverse, 1e-8);
+    return new SimpleEigenDecomposition(V, D, Vinverse);
+  }
+  
+  private static void test(SimpleMatrix matrix, SimpleMatrix v, SimpleMatrix d,
+      SimpleMatrix vinverse, double threshold)
+  {
+    EjmlUnitTests.assertEquals(matrix.getMatrix(), multiply(v.getMatrix(),d.getMatrix(),vinverse.getMatrix()).getMatrix(), threshold);
+  }
+
+  /**
+   * Returns the product of the three matrices.
+   * 
+   * TODO: in the future, this could be made more efficient by a factor of 2. 
+   * Currently use the naive method.
+   * 
+   * @param dense1 A dense matrix
+   * @param diag A diagonal matrix
+   * @param dense2 Another dense matrix
+   * @return
+   */
+  public static SimpleMatrix multiply(DenseMatrix64F dense1, DenseMatrix64F diag, DenseMatrix64F dense2)
+  {
+    SimpleMatrix 
+      d1 = new SimpleMatrix(dense1),
+      d = new SimpleMatrix(diag),
+      d2 = new SimpleMatrix(dense2);
+    return d1.mult(d).mult(d2);
+  }
+  
+  /**
+   * Holds the result of an eigendecomposition,
+   * 
+   * M = V * D * Vinverse
+   * 
+   * @author Alexandre Bouchard (alexandre.bouchard@gmail.com)
+   *
+   */
+  public static class SimpleEigenDecomposition
+  {
+    private final SimpleMatrix V, D, Vinverse;
+
+    private SimpleEigenDecomposition(SimpleMatrix v, SimpleMatrix d,
+        SimpleMatrix vinverse)
+    {
+      V = v;
+      D = d;
+      Vinverse = vinverse;
+    }
+
+    public SimpleMatrix getV()
+    {
+      return V;
+    }
+
+    public SimpleMatrix getD()
+    {
+      return D;
+    }
+
+    public SimpleMatrix getVinverse()
+    {
+      return Vinverse;
+    }
+
+    @Override
+    public String toString()
+    {
+      return "SimpleEigenDecomposition:\n" +
+        BriefStrings.indent(
+          "V:\n" + BriefStrings.indent(EJMLUtils.toString(V)) +
+          "\nD:\n" + BriefStrings.indent(EJMLUtils.toString(D)) +
+          "\nVinverse:\n" + BriefStrings.indent(EJMLUtils.toString(Vinverse)));
+    }
+  }
+  
+  /**
    * @param vector
    * @return
    */
-  public static double norm(SimpleMatrix vector)
+  public static double vectorL2NormSquared(SimpleMatrix vector)
   {
     if (vector.numCols() > 1) throw new RuntimeException();
     return vector.transpose().mult(vector).get(0,0);
   }
   
-  
+  /**
+   * Format a matrix, making sure the locale used is US.
+   * @param matrix
+   * @return
+   */
   public static String toString(SimpleMatrix matrix)
   {
     BriefIO.ensureUSLocale();
-    return matrix.toString().replaceFirst("^.*\\n", "");
+    return matrix.toString().replaceFirst("^.*\\n", "").replaceAll("\\n$", "");
   }
   
-  public static void main(String[] args)
+  public static void main(String [] args)
   {
-    SimpleMatrix test = new SimpleMatrix(2,2);
-    System.out.println(toString(test));
+    double [][] data = new double[][]{{1,1},{2,1}};
+    SimpleMatrix test = new SimpleMatrix(data);
+    System.out.println(simpleEigenDecomposition(test));
   }
+
 }
