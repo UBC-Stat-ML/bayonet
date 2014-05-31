2ae575aad41b78fcc3aefbdc96c543361c003916
Seong-Hwan Jun <s2jun.uw@gmail.com>
2014-05-31 11:44:04 -0700

    IntegerValuedVectorProcessor and TestFunction to fix the compilation error

---
+++ src/main/java/blang/variables/TestFunction.java
@@ -0,0 +1,11 @@
+package blang.variables;
+
+/**
+ * 
+ * @author Seong-Hwan Jun (s2jun.uw@gmail.com)
+ *
+ */
+public interface TestFunction 
+{
+	public double eval(RealVectorInterface vector);
+}
