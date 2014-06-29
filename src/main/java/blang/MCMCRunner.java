025f6397bbe98b4c7613e6c6bfecd3d02dcc3aa1
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-06-28 22:39:21 -0700

    Deprecating MCMCRunner
    See BlangSmallExample instead.

--- src/main/java/blang/MCMCRunner.java
+++ src/main/java/blang/MCMCRunner.java
@@ -10,7 +10,12 @@ import blang.processing.ProcessorFactory;
 import briefj.opt.OptionSet;
 
 
-
+/**
+ * 
+ * @author Alexandre Bouchard (alexandre.bouchard@gmail.com)
+ * @deprecated See BlangSmallExample for a better way of instrumenting
+ *   blang-based runs.
+ */
 public class MCMCRunner implements Runnable
 {
   @OptionSet(name = "factory")
