279db5f58e964f2947cc6625d0553973786f6b25
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-08-27 07:34:05 -0700

    Add some options to see R output more easily in RJavaBridge

--- src/main/java/bayonet/rplot/RJavaBridge.java
+++ src/main/java/bayonet/rplot/RJavaBridge.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import briefj.BriefFiles;
 import briefj.BriefIO;
+import briefj.BriefStrings;
 
 
 
@@ -12,9 +13,15 @@ public abstract class RJavaBridge
 {
   public abstract String rTemplateResourceURL();
   
+  /**
+   * Folder to put generated R script, csv used to transfer data,
+   * etc. If null, all these will be temporary.
+   */
+  private File workFolder = null;
+  
   public String javaDoublesToRVector(double [] numbers)
   {
-    File tempFile = BriefFiles.createTempFile();
+    File tempFile = tempFile(); 
     PrintWriter out = BriefIO.output(tempFile);
     for (double number : numbers)
       out.println(number);
@@ -22,9 +29,17 @@ public abstract class RJavaBridge
     return "scan('" + RUtils.escapeQuote(tempFile.getAbsolutePath()) + "')";
   }
   
+  private File tempFile()
+  {
+    if (workFolder == null)
+      return BriefFiles.createTempFile();
+    String id = BriefStrings.generateUniqueId();
+    return new File(workFolder, id);
+  }
+
   public String javaDoublesToRMatrix(double [][] numbers)
   {
-    File tempFile = BriefFiles.createTempFile();
+    File tempFile = tempFile(); //BriefFiles.createTempFile();
     PrintWriter out = BriefIO.output(tempFile);
     for (int r = 0; r < numbers.length; r++)
       for (int c = 0; c < numbers[0].length; c++)
@@ -32,4 +47,15 @@ public abstract class RJavaBridge
     out.close();
     return "matrix(scan('" + RUtils.escapeQuote(tempFile.getAbsolutePath()) + "'), " + numbers.length + ", " + numbers[0].length + ")";
   }
+
+  public File getWorkFolder()
+  {
+    return workFolder;
+  }
+
+  public void setWorkFolder(File workFolder)
+  {
+    this.workFolder = workFolder;
+  }
+
 }
--- src/main/java/bayonet/rplot/RUtils.java
+++ src/main/java/bayonet/rplot/RUtils.java
@@ -8,6 +8,7 @@ import org.mvel2.templates.TemplateRuntime;
 import binc.Command;
 import briefj.BriefFiles;
 import briefj.BriefIO;
+import briefj.BriefStrings;
 
 
 
@@ -17,24 +18,38 @@ public class RUtils
   
   public static Command Rscript = Command.cmd("Rscript");
   
-  
   public static String callR(String commands)
   {
-    File tempFile = BriefFiles.createTempFile();
-    BriefIO.write(tempFile, commands);
-    return Command.call(Rscript.withArgs(tempFile.getAbsolutePath()));
+    return callR(commands, null);
+  }
+  
+  public static String callR(String commands, File scriptFile)
+  {
+    if (scriptFile == null)
+      scriptFile = BriefFiles.createTempFile();
+    BriefIO.write(scriptFile, commands);
+    return Command.call(Rscript.withArgs(scriptFile.getAbsolutePath()));
   }
   
   public static String callGeneratedRScript(String templateResourceURL, Object context)
   {
+    return callGeneratedRScript(templateResourceURL, context, null);
+  }
+  
+  public static String callGeneratedRScript(String templateResourceURL, Object context, File scriptFile)
+  {
+    if (scriptFile == null)
+      scriptFile = BriefFiles.createTempFile();
     String template = BriefIO.resourceToString(templateResourceURL); 
     String generatedScript = (String) TemplateRuntime.eval(template, context);
-    return callR(generatedScript);
+    return callR(generatedScript, scriptFile);
   }
   
   public static String callRBridge(RJavaBridge bridge)
   {
-    return callGeneratedRScript(bridge.rTemplateResourceURL(), bridge);
+    File workFolder = bridge.getWorkFolder();
+    File scriptFile = workFolder == null ? null : new File(workFolder, "script-" + BriefStrings.generateUniqueId() + ".r");
+    return callGeneratedRScript(bridge.rTemplateResourceURL(), bridge, scriptFile);
   }
   
   public static void main(String [] args)
@@ -43,7 +58,7 @@ public class RUtils
     PlotHistogram.from(data).toPDF(new File("/Users/bouchard/temp/anotherhist.pdf"));
   }
   
-  static String escapeQuote(String str)
+  public static String escapeQuote(String str)
   {
     str = str.replace("\\", "\\\\");
     return str.replace("'", "\\'");
