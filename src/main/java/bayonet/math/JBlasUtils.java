f35f5e432dcbb17248da104d7f433eaaddca1abe
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2015-07-05 20:18:48 -0700

    Add utility in JBlas

--- src/main/java/bayonet/math/JBlasUtils.java
+++ src/main/java/bayonet/math/JBlasUtils.java
@@ -13,4 +13,12 @@ public class JBlasUtils
     return Solve.solvePositive(covar, DoubleMatrix.eye(covar.rows));
   }
 
+  public static double [][] asDoubleArray(DoubleMatrix matrix)
+  {
+    double [][] result = new double[matrix.rows][matrix.columns];
+    for (int r = 0; r < matrix.rows; r++)
+      for (int c = 0; c < matrix.columns; c++)
+        result[r][c] = matrix.get(r,c);
+    return result;
+  }
 }
